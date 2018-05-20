10efdb1b120ac79d173a37f7e4f96a2b7ff2e2bc
Mulx10 <mehulkumarnirala@iitkgp.ac.in>
2018-05-20 19:35:44 +0530

    complete v1.0

--- install.tor.sh
+++ install.tor.sh
@@ -1,21 +1,15 @@
-sudo apt install tor
 gpg --keyserver keys.gnupg.net --recv A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89
 gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo -E apt-key add -
 sudo apt update
+sudo apt install tor
 sudo apt install tor deb.torproject.org-keyring
-sudo bash -c 'echo "ControlPort 9051
-DataDirectory ~/.tor
-HTTPSProxy 172.16.2.30:8080
-HTTPProxy 172.16.2.30:8080
-#Socks5Proxy 172.16.2.30:8080
-Log notice stdout
-ReachableAddresses *:80,*:443
-ReachableAddresses reject *:*
-ReachableAddresses reject *:*
-ReachableAddresses reject *:*
-SocksPort 9050
-HashedControlPassword 16:405C6E032DE07FF0609BADA5BA92D9863A4F309442B486924B229987F2" > /etc/tor/torrc'
 sudo apt install screen
+
+sudo mv torrc /etc/tor/torrc
+
+sudo mv tunnel /usr/bin/tunnel
+sudo chmod 777 /usr/bin/tunnel
+
 bash -c 'echo "
 unproxy()
 {
@@ -26,21 +20,4 @@ unproxy()
     export all_proxy=""
 }
 " >> ~/.bashrc'
-#
-# Proxy tunel port 8008
-# conect browser at host:127.0.0.1 port:8008
-#
-echo "screen -dmS tor bash -c 'tor --HTTPTunnelPort 8008;exec bash'
-echo 'unproxy'>>~/.bashrc
-kill -9 \$(pidof tor) >/dev/null 2>/dev/null
-torify bash 
-head -n -1 ~/.bashrc > ~/temp ; mv ~/temp ~/.bashrc
-killall screen 2>/dev/null >/dev/null
-kill -9 \$(pidof tor) >/dev/null 2>/dev/null
-" > tor
 
-#
-# run bash with tor 
-# 
-sudo mv tor /usr/bin/tunnel
-sudo chmod 777 /usr/bin/tunnel
