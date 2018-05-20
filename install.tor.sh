bf47477091e702c47884915a65a2ccbd15924f13
Mulx10 <mehulkumarnirala@iitkgp.ac.in>
2018-05-20 20:06:21 +0530

    complete v1.0

--- install.tor.sh
+++ install.tor.sh
@@ -1,15 +1,17 @@
 gpg --keyserver keys.gnupg.net --recv A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89
 gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo -E apt-key add -
 sudo apt update
-sudo apt install tor
-sudo apt install tor deb.torproject.org-keyring
-sudo apt install screen
+sudo apt install -y tor
+sudo apt install -y deb.torproject.org-keyring
+sudo apt install -y screen
 
-sudo mv torrc /etc/tor/torrc
+sudo rm /etc/tor/torrc
+sudo cp torrc /etc/tor/torrc
 
-sudo mv tunnel /usr/bin/tunnel
+sudo rm /usr/bin/tunnel
+sudo cp tunnel /usr/bin/tunnel
 sudo chmod 777 /usr/bin/tunnel
-
+sudo /etc/init.d/tor start
 bash -c 'echo "
 unproxy()
 {
@@ -19,5 +21,5 @@ unproxy()
     export https_proxy=""
     export all_proxy=""
 }
-" >> ~/.bashrc'
+">> ~/.bashrc'
 
